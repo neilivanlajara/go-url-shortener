da55eaa7bab61a9cb93f0fc478f63417dda0be97
ivan.lajara <neilivanlajara@gmail.com>
2026-01-14 01:33:46 +0100

    Add basic HTTP server with health check

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
 func main() {
+    r := gin.Default()
+    
+    r.GET("/health", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "status": "healthy",
+        })
+    })
+    
+    r.Run(":8080")
 }
